7f527868b4c020ad9df6907509565f5441f1ac20
dsb <754739481@qq.com>
2018-11-22 15:15:06 +0800

    条形码

--- WebApp/src/main/java/com/dsb/util/BarcodeUtil.java
+++ WebApp/src/main/java/com/dsb/util/BarcodeUtil.java
@@ -3,22 +3,29 @@ package com.dsb.util;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 
-import org.apache.commons.fileupload.disk.DiskFileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.io.IOUtils;
+import javax.imageio.ImageIO;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.krysalis.barcode4j.HumanReadablePlacement;
+import org.krysalis.barcode4j.impl.code128.Code128Bean;
 import org.krysalis.barcode4j.impl.code39.Code39Bean;
 import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
 import org.krysalis.barcode4j.tools.UnitConv;
-import org.springframework.http.MediaType;
 
 import com.alibaba.dubbo.common.utils.StringUtils;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
  * 条形码工具类
@@ -28,6 +35,9 @@ import com.alibaba.dubbo.common.utils.StringUtils;
  *
  */
 public class BarcodeUtil {
+    private static final String CODE = "utf-8";
+    private static final int BLACK = 0xff000000;
+    private static final int WHITE = 0xFFFFFFFF;
 
     /**
      * 生成文件
@@ -97,20 +107,191 @@ public class BarcodeUtil {
             throw new RuntimeException(e);
         }
     }
-
-    public static void main(String[] args) {
-        String msg = "0212359254823655";
-        String path = "barcode.png";
-        generateFile(msg, path);
-        
-        
-        // File 转 FileItem
-        File file = new File("E:\\barcode.png");
-        DiskFileItem fileItem = (DiskFileItem) new DiskFileItemFactory().createItem("file", MediaType.TEXT_PLAIN_VALUE, true, "barcode.png");
-        try (InputStream input = new FileInputStream(file); OutputStream os = fileItem.getOutputStream()) {
-            IOUtils.copy(input, os);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Invalid file: " + e, e);
+    /**
+     * 生成code128条形码
+     *
+     * @param height        条形码的高度
+     * @param width         条形码的宽度
+     * @param message       要生成的文本
+     * @param withQuietZone 是否两边留白
+     * @param hideText      隐藏可读文本
+     * @return 图片对应的字节码
+     */
+    public static byte[] generateBarCode128(String message, Double height, Double width, boolean withQuietZone, boolean hideText) {
+        Code128Bean bean = new Code128Bean();
+        // 分辨率
+        int dpi = 512;
+        // 设置两侧是否留白
+        bean.doQuietZone(withQuietZone);
+ 
+        // 设置条形码高度和宽度
+        bean.setBarHeight((double) ObjectUtils.defaultIfNull(height, 9.0D));
+        if (width != null) {
+            bean.setModuleWidth(width);
         }
+        // 设置文本位置（包括是否显示）
+        if (hideText) {
+            bean.setMsgPosition(HumanReadablePlacement.HRP_NONE);
+        }
+        // 设置图片类型
+        String format = "image/png";
+ 
+        ByteArrayOutputStream ous = new ByteArrayOutputStream();
+        BitmapCanvasProvider canvas = new BitmapCanvasProvider(ous, format, dpi,
+                BufferedImage.TYPE_BYTE_BINARY, false, 0);
+ 
+        // 生产条形码
+        bean.generateBarcode(canvas, message);
+        try {
+            canvas.finish();
+        } catch (IOException e) {
+            //ByteArrayOutputStream won't happen
+        }
+ 
+        return ous.toByteArray();
+    }
+
+    // 将byte数组写入文件
+    public static void createFile(String path, byte[] content) throws IOException {
+
+        FileOutputStream fos = new FileOutputStream(path);
+
+        fos.write(content);
+        fos.close();
+    }
+
+    /**
+     * 生成一维码（128）
+     *
+     * @param str
+     * @param width
+     * @param height
+     * @return
+     * @author wuhongbo
+     */
+    public static BufferedImage getBarcode(String str, Integer width, Integer height) {
+
+        if (width == null || width < 200) {
+            width = 200;
+        }
+
+        if (height == null || height < 50) {
+            height = 50;
+        }
+
+        try {
+            // 文字编码
+            Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+            hints.put(EncodeHintType.CHARACTER_SET, CODE);
+            hints.put(EncodeHintType.MARGIN, 0);
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(str, BarcodeFormat.CODE_128, width, height, hints);
+            
+            return toBufferedImage(bitMatrix);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 同步创建条形码图片
+     *
+     * @param content
+     *            要生成条形码包含的内容
+     * @param width
+     *            条形码的宽度，单位px
+     * @param height
+     *            条形码的高度，单位px
+     * @return 返回生成条形的位图
+     *
+     *         白边问题: https://blog.csdn.net/sunshinwong/article/details/50156017
+     *         已知高度,计算宽度:
+     *
+     */
+    private static BufferedImage syncEncodeBarcode(String content, int width, int height) {
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.MARGIN, 0);
+
+        try {
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.CODE_128, width, height, hints);
+            int[] pixels = new int[width * height];
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * width + x] = 0xff000000;
+                    } else {
+                        pixels[y * width + x] = 0xffffffff;
+                    }
+                }
+            }
+            return toBufferedImage(bitMatrix);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }    
+    /**
+     * 转换成图片
+     *
+     * @param matrix
+     * @return
+     * @author wuhongbo
+     */
+    private static BufferedImage toBufferedImage(BitMatrix matrix) {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, matrix.get(x, y) ? BLACK : WHITE);
+            }
+        }
+        return image;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String msg = "6921804700757";
+        String path = "E:\\barcode.png";
+////        generateFile(msg, path);
+//        
+//        byte[] content = generateBarCode128(msg, 0.2d, 0.5d, false, true);
+//        createFile(path, content);
+        
+//        
+//        // File 转 FileItem
+//        File file = new File("E:\\barcode.png");
+//        DiskFileItem fileItem = (DiskFileItem) new DiskFileItemFactory().createItem("file", MediaType.TEXT_PLAIN_VALUE, true, "barcode.png");
+//        try (InputStream input = new FileInputStream(file); OutputStream os = fileItem.getOutputStream()) {
+//            IOUtils.copy(input, os);
+//        } catch (Exception e) {
+//            throw new IllegalArgumentException("Invalid file: " + e, e);
+//        }
+        
+        BufferedImage image = getBarcode("1000000000000028", null, null);
+//        BufferedImage image = syncEncodeBarcode("100000000003328", 200, 50);
+        ImageIO.write(image, "png", new File(path));
+        
+
+//        //Generate
+//        //Create the BarcodeSettings
+//        BarcodeSettings settings = new BarcodeSettings();
+//        //Set Data
+//        settings.setData("1000000000000028");
+//        //Set the Symbology property
+//        settings.setType(BarCodeType.CODE_128);
+//        //Set ShowText location on bottom
+//        settings.setShowTextOnBottom(true);
+//        //Set Border is visible
+//        settings.hasBorder(true);
+//        //Create the BarCodeGenerator
+//        BarCodeGenerator barCodeGenerator = new BarCodeGenerator(settings);
+//        //Get image from the BarCodeGenerator
+//        BufferedImage bufferedImage = barCodeGenerator.generateImage();
+//        //Save the image
+//        ImageIO.write(bufferedImage,"png",new File("E:\\CODE_128.png"));
+
     }
 }
